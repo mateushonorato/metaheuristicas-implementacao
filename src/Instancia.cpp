edca662e7218b7aceda80fd5f41d3babcefd3844
Mateus Honorato <mateush.honorato@gmail.com>
2024-07-24 18:02:01 -0300

    melhorias na formatação e avisos no carregamento automático da instância

--- src/Instancia.cpp
+++ src/Instancia.cpp
@@ -1,42 +1,47 @@
 #include "Instancia.h"
 
-void calculaDistancias(Instancia& inst)
+void calculaDistancias(Instancia &inst)
 {
-    for(int i = 0; i < inst.n; i++)
+    for (int i = 0; i < inst.n; i++)
     {
-        for(int j = 0; j < inst.n; j++)
+        for (int j = 0; j < inst.n; j++)
         {
             inst.distancia[i][j] = sqrt(pow(inst.coords[i].x - inst.coords[j].x, 2) + pow(inst.coords[i].y - inst.coords[j].y, 2));
         }
     }
-
 }
 
-void leInstancia(Instancia& inst, string nome_arq)
+void leInstancia(Instancia &inst, string nomeArq)
 {
-    ifstream arq(nome_arq);
-    if(arq.is_open())
+    ifstream arq(nomeArq);
+    if (arq.is_open())
     {
         // número de cidades
         arq >> inst.n;
 
         // criando o vetor de pontos
-        inst.coords = new Ponto[inst.n];        
+        inst.coords = new Ponto[inst.n];
 
-        //lendo as coordenadas
-        for(int i = 0; i < inst.n; i++)
+        // lendo as coordenadas
+        for (int i = 0; i < inst.n; i++)
         {
             arq >> inst.coords[i].id >> inst.coords[i].x >> inst.coords[i].y;
         }
 
-        //criando a matriz de distâncias
-        inst.distancia = new float*[inst.n];
-        for(int i = 0; i < inst.n; i++)
+        // criando a matriz de distâncias
+        inst.distancia = new float *[inst.n];
+        for (int i = 0; i < inst.n; i++)
         {
             inst.distancia[i] = new float[inst.n];
         }
 
-        //calculando a matriz
+        // calculando a matriz
         calculaDistancias(inst);
+
+        cout << "Instância " << nomeArq << " carregada com sucesso." << endl << endl;
+    }
+    else
+    {
+        cerr << "Erro ao abrir o arquivo!" << endl;
     }
 }
